0af0f9057e06d00350bd54f97dcaf7fe7a1c3311
Niklas Higi <niklas@shroudedcode.com>
2019-01-02 21:02:20 +0100

    Fix typo in type declarations

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare class SimpleGPIO {
   constructor()
 
   setupInput(pin: number)
-  setupOuput(pin: number)
+  setupOutput(pin: number)
   read(pin: number): boolean
   write(pin: number, value: boolean): void
 }
